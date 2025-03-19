3c540bb100b361955e2de16fd4cdfe4f89079b3b
rayenDC <rayenbelhadj01@gmail.com>
2025-03-19 12:44:51 +0100

    start point

---
+++ src/app/add-edit-supplier/add-edit-supplier.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-edit-supplier',
+  imports: [],
+  templateUrl: './add-edit-supplier.component.html',
+  styleUrl: './add-edit-supplier.component.css'
+})
+export class AddEditSupplierComponent {
+
+}
---
+++ src/app/sell/sell.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sell',
+  imports: [],
+  templateUrl: './sell.component.html',
+  styleUrl: './sell.component.css'
+})
+export class SellComponent {
+
+}
