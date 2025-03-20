29069ca6b2fb9c9fd2abd6b3866c646fb7c4e0fe
rayenDC <rayenbelhadj01@gmail.com>
2025-03-20 17:00:29 +0100

    texting git hub after the change

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -11,7 +11,7 @@ import {Observable} from "rxjs";
 })
 export class ApiService {
   private static BASE_URL = 'http://localhost:8080/api';
-  private static ENCRYPTION_KEY = 'my-encrytion-key';
+  private static ENCRYPTION_KEY = 'phegon-dev-inventory';
 
   authStatusChanged = new EventEmitter<void>();
 
@@ -43,5 +43,181 @@ export class ApiService {
   private clearAuth() {
     localStorage.removeItem('token');
     localStorage.removeItem('role');
+  }
+
+  private getHeader(): HttpHeaders {
+    const token = this.getFromStorageAndDecrypt('token');
+    return new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+    });
+  }
+
+  /**CATEGOTY ENDPOINTS */
+  createCategory(body: any): Observable<any> {
+    return this.http.post(`${ApiService.BASE_URL}/categories/add`, body, {
+      headers: this.getHeader(),
+    });
+  }
+
+  getAllCategory(): Observable<any> {
+    return this.http.get(`${ApiService.BASE_URL}/categories/all`, {
+      headers: this.getHeader(),
+    });
+  }
+
+  getCategoryById(id: string): Observable<any> {
+    return this.http.get(`${ApiService.BASE_URL}/categories/${id}`, {
+      headers: this.getHeader(),
+    });
+  }
+
+  updateCategory(id: string, body: any): Observable<any> {
+    return this.http.put(
+      `${ApiService.BASE_URL}/categories/update/${id}`,
+      body,
+      {
+        headers: this.getHeader(),
+      }
+    );
+  }
+
+  deleteCategory(id: string): Observable<any> {
+    return this.http.delete(`${ApiService.BASE_URL}/categories/delete/${id}`, {
+      headers: this.getHeader(),
+    });
+  }
+
+  /** SUPPLIER API */
+
+  addSupplier(body: any): Observable<any> {
+    return this.http.post(`${ApiService.BASE_URL}/suppliers/add`, body, {
+      headers: this.getHeader(),
+    });
+  }
+
+  getAllSuppliers(): Observable<any> {
+    return this.http.get(`${ApiService.BASE_URL}/suppliers/all`, {
+      headers: this.getHeader(),
+    });
+  }
+
+  getSupplierById(id: string): Observable<any> {
+    return this.http.get(`${ApiService.BASE_URL}/suppliers/${id}`, {
+      headers: this.getHeader(),
+    });
+  }
+
+  updateSupplier(id: string, body: any): Observable<any> {
+    return this.http.put(
+      `${ApiService.BASE_URL}/suppliers/update/${id}`,
+      body,
+      {
+        headers: this.getHeader(),
+      }
+    );
+  }
+
+  deleteSupplier(id: string): Observable<any> {
+    return this.http.delete(`${ApiService.BASE_URL}/suppliers/delete/${id}`, {
+      headers: this.getHeader(),
+    });
+  }
+
+  /**PRODUICTS ENDPOINTS */
+  addProduct(formData: any): Observable<any> {
+    return this.http.post(`${ApiService.BASE_URL}/products/add`, formData, {
+      headers: this.getHeader(),
+    });
+  }
+
+  updateProduct(formData: any): Observable<any> {
+    return this.http.put(`${ApiService.BASE_URL}/products/update`, formData, {
+      headers: this.getHeader(),
+    });
+  }
+
+  getAllProducts(): Observable<any> {
+    return this.http.get(`${ApiService.BASE_URL}/products/all`, {
+      headers: this.getHeader(),
+    });
+  }
+
+  getProductById(id: string): Observable<any> {
+    return this.http.get(`${ApiService.BASE_URL}/products/${id}`, {
+      headers: this.getHeader(),
+    });
+  }
+
+  deleteProduct(id: string): Observable<any> {
+    return this.http.delete(`${ApiService.BASE_URL}/products/delete/${id}`, {
+      headers: this.getHeader(),
+    });
+  }
+
+  /**Transactions Endpoints */
+
+  purchaseProduct(body: any): Observable<any> {
+    return this.http.post(
+      `${ApiService.BASE_URL}/transactions/purchase`,
+      body,
+      {
+        headers: this.getHeader(),
+      }
+    );
+  }
+
+  sellProduct(body: any): Observable<any> {
+    return this.http.post(`${ApiService.BASE_URL}/transactions/sell`, body, {
+      headers: this.getHeader(),
+    });
+  }
+
+  getAllTransactions(searchText: string): Observable<any> {
+    return this.http.get(`${ApiService.BASE_URL}/transactions/all`, {
+      params: { searchText: searchText },
+      headers: this.getHeader(),
+    });
+  }
+
+  getTransactionById(id: string): Observable<any> {
+    return this.http.get(`${ApiService.BASE_URL}/transactions/${id}`, {
+      headers: this.getHeader(),
+    });
+  }
+
+  updateTransactionStatus(id: string, status: string): Observable<any> {
+    return this.http.put(
+      `${ApiService.BASE_URL}/transactions/update/${id}`,
+      JSON.stringify(status),
+      {
+        headers: this.getHeader().set('Content-Type', 'application/json'),
+      }
+    );
+  }
+
+  getTransactionsByMonthAndYear(month: number, year: number): Observable<any> {
+    return this.http.get(`${ApiService.BASE_URL}/transactions/by-month-year`, {
+      headers: this.getHeader(),
+      params: {
+        month: month,
+        year: year,
+      },
+    });
+  }
+
+  /**AUTHENTICATION CHECKER */
+
+  logout(): void {
+    this.clearAuth();
+  }
+
+  isAuthenticated(): boolean {
+    const token = this.getFromStorageAndDecrypt('token');
+    return !!token;
+  }
+
+  isAdmin(): boolean {
+    const role = this.getFromStorageAndDecrypt('role');
+    return role === 'ADMIN';
   }
 }
